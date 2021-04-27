39d398d54b93c89b1d54308e51e8f3ec72240d52
robotics <roboticsdynamicsb4@gmail.com>
2021-04-27 14:08:50 +0900

    a

--- ABC199/main_c.py
+++ ABC199/main_c.py
@@ -1,24 +1,42 @@
+# n = int(input())
+# s = input()
+# q = int(input())
+# src = []
+
+
+
+# for i in range(q):
+#     src.append(list(map(int, input().split())))
+
+# for i in range(q):
+#     if src[i][0] == 1:
+#         a = src[i][1] - 1
+#         b = src[i][2] - 1
+#         s = s[0:a] + s[b] + s[a + 1:b] + s[a] + s[b + 1:2 * n]
+
+#     elif src[i][0] == 2:
+#         # s[0:n], s[n+1:2 * n] = s[n+1:2 * n], s[0:n]
+#         front = s[0:n]
+#         back = s[n : n*2]
+#         s = back + front
+
+# print(s)
+
 n = int(input())
-s = input()
+s = list(input())
 q = int(input())
-src = []
-
-
-
 for i in range(q):
-    src.append(list(map(int, input().split())))
-
-for i in range(q):
-    if src[i][0] == 1:
-        a = src[i][1] - 1
-        b = src[i][2] - 1
-        s = s[0:a] + s[b] + s[a + 1:b] + s[a] + s[b + 1:2 * n]
-
-    elif src[i][0] == 2:
-        # s[0:n], s[n+1:2 * n] = s[n+1:2 * n], s[0:n]
-        front = s[0:n]
+    t, a, b = map(int, input().split())
+    if t == 1:
+        a -= 1
+        b -= 1
+        s[a], s[b] = s[b], s[a]
+    
+    elif t == 2:
+        front = s[0 : n]
         back = s[n : n*2]
         s = back + front
 
-print(s)
+s = "".join(s)
 
+print(s)
